8fd289ab2925dbed8076b83efc22eb2fcb560f1b
Maicon <maiconlsilva10@gmail.com>
2022-12-08 12:27:45 -0300

    creating balance

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import Header from "../../components/Header";
+import Balance from "../../components/Balance";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Header name={"Maicon"} />
-      <Text>My app</Text>
-
+      <Header name='Maicon' />
+      <Balance saldo='9.250,00' gastos='-527,00' />
       <StatusBar style='auto' />
     </View>
   );
@@ -17,6 +18,6 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#fafafa",
   },
 });
